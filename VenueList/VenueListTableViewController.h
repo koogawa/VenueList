cba2029f0711562db98dfd4934fcfc947ee9168d
koogawa <koogawa.app@gmail.com>
2011-11-18 01:53:24 +0900

    First Commit

---
+++ VenueList/VenueListTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  VenueListTableViewController.h
+//  VenueList
+//
+//  Created by Kosuke Ogawa on 11/11/11.
+//  Copyright (c) 2011年 Kosuke Ogawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VenueListTableViewController : UITableViewController
+{
+    NSMutableArray *venues_;
+}
+
+@end
---
+++ VenueList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VenueList
+//
+//  Created by 小川 航佑 on 11/11/11.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
